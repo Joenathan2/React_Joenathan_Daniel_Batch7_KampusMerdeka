6d08fbe278606fc11551d4c554c464e60252cb01
Joenathan <joenathandaniel33@gmail.com>
2024-10-13 15:54:00 +0700

    JavaScript

--- 07_JavaScript/Praktikum/script.js
+++ 07_JavaScript/Praktikum/script.js
@@ -1,33 +1,27 @@
 function swapValues1(a, b) {
-    let temp = a; 
-    a = b; 
-    b = temp; 
+  let temp = a;
+  a = b;
+  b = temp;
 
-    console.log('a:', a, 'b:', b);
+  console.log("a:", a, "b:", b);
 }
-swapValues1(5, 10); 
-
+swapValues1(5, 10);
 
 function swapValues2(a, b) {
-   
-    var temp = a; 
-    a = b; 
-    b = temp; 
+  var temp = a;
+  a = b;
+  b = temp;
 
-
-    console.log('a:', a, 'b:', b);
+  console.log("a:", a, "b:", b);
 }
-swapValues2(5, 10); 
-
+swapValues2(5, 10);
 
 function swapValues3(a, b) {
-                
-    const temp = a; 
-    a = b; 
-    b = temp; 
+  const temp = a;
+  a = b;
+  b = temp;
 
-   
-    console.log('a:', a, 'b:', b);
+  console.log("a:", a, "b:", b);
 }
-swapValues3(5, 10); 
+swapValues3(5, 10);
 //Sebaiknya Gunakan const, karena membantu menjaga nilai tetap stabil, mencegah kesalahan, dan membuat kode lebih jelas. (saya mengambil dari forum dan saya simpulkan)
